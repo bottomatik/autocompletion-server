a60ea5903877268ff735334c3c7dc7ed2970aec1
Jo Colina <jo@jocolina.com>
2017-09-23 22:30:32 +0200

    Intial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Autocomplete Bottomatik
+## A simple autocompletion server based on express capable of separating completions by system
+
+#### Installation
+Clone this repository and run:
+
+`npm install`
+
+The only requirements are `express`, `cors` and `body-parser`.
+
+## API
+
+All requests are performed using `Content-Type: application/json` .
+
+### `POST` `/:id`
+
+Requires autocompletion data.
+
+| Parameters | Required | Type |Description |
+|:----------:|:--------:|:----:|:----------:|
+| `query` | `true` | String | The query. Example: `What is my` |
+
+
+### `POST` `/new/:id`
+
+Creates a new system to work on.
+Note that systems are immutable. Feel free to add a PUT request to add to current array.
+
+| Parameters | Required | Type |Description |
+|:----------:|:--------:|:----:|:----------:|
+| `data` | `true` | Array | All possible autocompletions. Example: ["What's my social secutiry number", "What's my IP address"]|
+
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+
+app.options('*', cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const Systems = require('./lib/systems');
+const Autocomplete = require('./lib/autocomplete');
+
+app.get('/', function (req, res, err){
+	return res.status(400).json({
+		error:{
+			message: 'System needed'
+		}
+	});
+});
+
+app.post('/:id', function(req, res, err){
+	if(!req.body.query){
+		return res.status(400).error({error:{message:'Query required in body'}});
+	}
+
+	try{
+		let system = Systems.get(req.params.id);
+		if(!system){
+			return res.status(404).json({error:{message:'System does not exist'}});
+		}
+		return res.json(Autocomplete.get(req.body.query, system));
+	} catch(err) {
+		console.error(err);
+		return res.status(500).json({error:{message:err.message}});	
+	}
+});
+
+app.post('/new/:id', function(req, res, next){
+	if(!req.body.data){
+		return res.status(400).error({error:{message:'Data required in body'}});
+	}
+
+	try{
+		Systems.new(req.params.id, req.body.data);
+		return res.json({success:true});	
+	} catch(e) {
+		return res.status(500).json({error:{message:e.message}});
+	}
+});
+
+app.listen(7523, function(){
+	console.log('Server listening on port 7523');
+});
